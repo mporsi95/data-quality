e8adaedf2526d3749b922a8be2ce37af97df6270
mporsi95 <matheusorsi@gmail.com>
2024-03-16 19:10:40 -0300

    corrige problema de overhead de memória e inicia tratamentos nos csv's

--- notebooks/dhzlib.py
+++ notebooks/dhzlib.py
@@ -4,9 +4,23 @@ from pyspark.sql import SparkSession, DataFrame
 
 class dhzlib():
     def __init__(self) -> None:
-        self.spark = SparkSession.builder.appName("dhzlib").getOrCreate()
+        self.spark = self.create_spark_session()
         self.load_env()
     
+    def create_spark_session(self) -> SparkSession:
+        print('Creating Spark session')
+        try:
+            spark =  SparkSession.builder \
+                .master('local[*]') \
+                .appName('dhzlib') \
+                .config('spark.executor.memory', '8g') \
+                .config('spark.driver.memory', '8g') \
+                .getOrCreate()
+            print(f'SparkSession created at {spark.sparkContext._jsc.sc().uiWebUrl().get()}')
+            return spark
+        except Exception as e:
+            print(e)
+
     def load_env(self):
         try:
             loaded = load_dotenv(verbose=False)
@@ -47,9 +61,10 @@ class dhzlib():
         try:
             properties = {
                 'user': os.getenv('MYSQL_USER'),
-                'password': os.getenv('MYSQL_PASSWORD')
+                'password': os.getenv('MYSQL_PASSWORD'),
+                'driver': 'com.mysql.cj.jdbc.Driver'
             }
-            df = self.spark.read.jdbc(os.getenv('MYSQL_URL'), table, properties=properties)
+            df = self.spark.read.jdbc(os.getenv('MYSQL_URL'), table, properties=properties).persist()
             print(f'Data read from MySQL')
             return df
         except Exception as e:
