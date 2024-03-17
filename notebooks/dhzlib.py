f33a7bfeac3c806729901a1ba140802237f22637
mporsi95 <matheusorsi@gmail.com>
2024-03-17 20:57:30 -0300

    adiciona função de integridade de dados no intervalo mencionado

--- notebooks/dhzlib.py
+++ notebooks/dhzlib.py
@@ -1,12 +1,16 @@
 import os
-from datetime import datetime
+import warnings
+import pandas as pd
+
+from datetime import datetime, date
 from dotenv import load_dotenv
 from pyspark.sql import SparkSession, DataFrame
+from pyspark.sql import functions as F
 
 class DhzLib():
     def __init__(self) -> None:
         self.spark = self.create_spark_session()
-        self.datetime_ = datetime
+        self.date_ = date
         self.load_env()
     
     def create_spark_session(self) -> SparkSession:
@@ -18,6 +22,7 @@ class DhzLib():
                 .config('spark.executor.memory', '8g') \
                 .config('spark.driver.memory', '8g') \
                 .getOrCreate()
+            spark.conf.set("spark.sql.session.timeZone", "America/Sao_Paulo")
             print(f'SparkSession created at {spark.sparkContext._jsc.sc().uiWebUrl().get()}')
             return spark
         except Exception as e:
@@ -37,13 +42,40 @@ class DhzLib():
 # Data Quality Functions
 ############################################
 
-    def check_interval_integrity(self, df: DataFrame, date_column: str) -> None:
+    def check_interval_integrity(self,
+                           df: DataFrame,
+                           date_column: str,
+                           lower_bound: datetime,
+                           upper_bound: datetime) -> None:
         try:
-            distinct_days = df.select(date_column).distinct().count()
-            print(f'Distinct days: {distinct_days}')
+            print(f'Checking interval integrity for column {date_column}')
+            print(f'Interval: {lower_bound.strftime("%Y-%m-%d")} to {upper_bound.strftime("%Y-%m-%d")}')
+
+            days_between = (upper_bound - lower_bound).days + 1
+            filtered_df = df.filter(F.col(date_column).cast('date').between(lower_bound, upper_bound))
+            distinct_days = filtered_df.select(F.col(date_column).cast('date')).distinct().count()
+            
+            if distinct_days != days_between:
+                expected_dates = set(pd.date_range(start=lower_bound,
+                                                    end=upper_bound,
+                                                    freq='d') \
+                                        .map(lambda x: datetime.date(x)).tolist())
+                
+                actual_dates = filtered_df.select(F.col(date_column).cast('date')).distinct().toPandas()[date_column].tolist()
+                dates_missing = expected_dates.symmetric_difference(actual_dates)
+
+                warnings.warn(f'''
+                            Integrity check failed for {date_column}.
+                            Expected {days_between} days, but got {distinct_days} days.
+                            Dates missing: {[date.strftime("%Y-%m-%d") for date in dates_missing]}
+                        ''')
+                return
+            
+            print(f'Integrity check passed for {date_column}')
             return
         except Exception as e:
             print(e)
+            return
 
 
 
