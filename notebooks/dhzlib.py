c56befa2f69d20cf1cf511304dd0991578412b5e
mporsi95 <matheusorsi@gmail.com>
2024-04-08 09:54:38 -0300

    adiciona generate_stats_table

--- notebooks/dhzlib.py
+++ notebooks/dhzlib.py
@@ -4,7 +4,6 @@ import pandas as pd
 import findspark
 
 from datetime import datetime, date
-from dotenv import load_dotenv
 from pyspark.sql import SparkSession, DataFrame
 from pyspark.sql import functions as F
 from pyspark.sql.types import StructType, StructField, StringType, DateType, LongType, DoubleType
@@ -15,7 +14,6 @@ class DhzLib():
     def __init__(self) -> None:
         self.spark = self.create_spark_session()
         self.date_ = date
-        self.load_env()
 
 ############################################
 # Class initialization functions
@@ -33,16 +31,6 @@ class DhzLib():
             spark.conf.set("spark.sql.session.timeZone", "America/Sao_Paulo")
             print(f'SparkSession created at {spark.sparkContext._jsc.sc().uiWebUrl().get()}')
             return spark
-        except Exception as e:
-            print(e)
-
-    def load_env(self):
-        try:
-            loaded = load_dotenv(verbose=False)
-            if loaded:
-                return "Environment variables loaded"
-            else:
-                raise Exception("Error loading environment variables")
         except Exception as e:
             print(e)
 
@@ -86,83 +74,6 @@ class DhzLib():
             print(e)
             return
         
-    # def check_null_values(self, df: DataFrame) -> None:
-    #     try:
-    #         nulls = df.select([F.count(F.when(F.isnull(c), c)).alias(c) for c in df.columns]).collect()
-    #         print('Nulls check')
-    #         print(nulls)
-    #         return
-    #     except Exception as e:
-    #         print(e)
-    #         return
-
-
-    # def check_duplicates(self, df: DataFrame) -> None:
-    #     try:
-    #         duplicates = df.groupBy(df.columns).count().filter('count > 1').collect()
-    #         print('Duplicates check')
-    #         print(duplicates)
-    #         return
-    #     except Exception as e:
-    #         print(e)
-   
-
-
-############################################
-# Database Helper Functions
-############################################
-
-    def test_connection_mysql(self) -> None:
-        try:
-            properties = {
-                'user': os.getenv('MYSQL_USER'),
-                'password': os.getenv('MYSQL_PASSWORD')
-            }
-            test_connection = self.spark.read.jdbc(os.getenv('MYSQL_URL_UTILS'), 'teste_conexao', properties=properties)
-            if not test_connection:
-                raise Exception("Error connecting to MySQL")
-            print('Connected to MySQL')
-            return
-        except Exception as e:
-            print(e)
-    
-    def write_mysql(self, df: DataFrame, table: str) -> None:
-        try:
-            properties = {
-                'user': os.getenv('MYSQL_USER'),
-                'password': os.getenv('MYSQL_PASSWORD')
-            }
-            df.write.jdbc(os.getenv('MYSQL_URL'), table, properties=properties)
-            print('Data written to MySQL')
-            return
-        except Exception as e:
-            print(e)
-
-    def write_mysql_utils(self, df: DataFrame, table: str, mode_: str) -> None:
-        try:
-            properties = {
-                'user': os.getenv('MYSQL_USER'),
-                'password': os.getenv('MYSQL_PASSWORD')
-            }
-            df.write.jdbc(os.getenv('MYSQL_URL_UTILS'), table, properties=properties, mode=mode_)
-            print('Data written to MySQL')
-            return
-        except Exception as e:
-            print(e)
-
-    def read_mysql(self, table: str) -> DataFrame:
-        try:
-            properties = {
-                'user': os.getenv('MYSQL_USER'),
-                'password': os.getenv('MYSQL_PASSWORD'),
-                'driver': 'com.mysql.cj.jdbc.Driver'
-            }
-            df = self.spark.read.jdbc(os.getenv('MYSQL_URL'), table, properties=properties).persist()
-            print(f'Data read from MySQL')
-            return df
-        except Exception as e:
-            print(e)
-    
     def generate_stats_table(self, tables: dict) -> None:
         try:
             stats_df = []
@@ -207,3 +118,86 @@ class DhzLib():
             return
         except Exception as e:
             print(e)
+        
+    # def check_null_values(self, df: DataFrame) -> None:
+    #     try:
+    #         nulls = df.select([F.count(F.when(F.isnull(c), c)).alias(c) for c in df.columns]).collect()
+    #         print('Nulls check')
+    #         print(nulls)
+    #         return
+    #     except Exception as e:
+    #         print(e)
+    #         return
+
+
+    # def check_duplicates(self, df: DataFrame) -> None:
+    #     try:
+    #         duplicates = df.groupBy(df.columns).count().filter('count > 1').collect()
+    #         print('Duplicates check')
+    #         print(duplicates)
+    #         return
+    #     except Exception as e:
+    #         print(e)
+   
+
+
+############################################
+# Database Helper Functions
+############################################
+
+    def test_connection_mysql(self) -> None:
+        try:
+            properties = {
+                'user': os.getenv('MYSQL_USER'),
+                'password': os.getenv('MYSQL_PASSWORD')
+            }
+            test_connection = self.spark.read.jdbc(os.getenv('MYSQL_URL_UTILS'), 'teste_conexao', properties=properties)
+
+            if not test_connection:
+                raise Exception("Error connecting to MySQL")
+            
+            print('Connected to MySQL')
+            return
+        except Exception as e:
+            print(e)
+    
+    def write_mysql(self, df: DataFrame, table: str) -> None:
+        try:
+            properties = {
+                'user': os.getenv('MYSQL_USER'),
+                'password': os.getenv('MYSQL_PASSWORD')
+            }
+            df.write.jdbc(os.getenv('MYSQL_URL'), table, properties=properties)
+
+            print('Data written in MySQL')
+            return
+        except Exception as e:
+            print(e)
+
+    def write_mysql_utils(self, df: DataFrame, table: str, mode_: str) -> None:
+        try:
+            properties = {
+                'user': os.getenv('MYSQL_USER'),
+                'password': os.getenv('MYSQL_PASSWORD')
+            }
+            df.write.jdbc(os.getenv('MYSQL_URL_UTILS'), table, properties=properties, mode=mode_)
+
+            print('Data written in MySQL')
+            return
+        except Exception as e:
+            print(e)
+
+    def read_mysql(self, table: str) -> DataFrame:
+        try:
+            properties = {
+                'user': os.getenv('MYSQL_USER'),
+                'password': os.getenv('MYSQL_PASSWORD'),
+                'driver': 'com.mysql.cj.jdbc.Driver'
+            }
+            df = self.spark.read.jdbc(os.getenv('MYSQL_URL'), table, properties=properties).persist()
+            print(f'Data read from MySQL')
+            return df
+        except Exception as e:
+            print(e)
+    
+    
